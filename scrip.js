71fe6bc8196f1261796e0b64b5e1899e54ac8bf6
Made-hern <madelinrebecahernandezaguilar@gmail.com>
2026-06-04 15:21:08 -0600

    pruebas

--- scrip.js
+++ scrip.js
@@ -1,13 +1,34 @@
+// MENU HAMBURGUESA
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links a");
 
-menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
-
-links.forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
+if(menuToggle){
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
     });
+}
+
+// ANIMACION DE CARGA
+window.addEventListener("load", () => {
+
+    const loader = document.querySelector(".loader");
+    const contenido = document.getElementById("contenido");
+
+    if(loader && contenido){
+
+        setTimeout(() => {
+
+            loader.style.opacity = "0";
+
+            setTimeout(() => {
+
+                loader.style.display = "none";
+                contenido.classList.add("show");
+
+            }, 800);
+
+        }, 3000);
+
+    }
+
 });
